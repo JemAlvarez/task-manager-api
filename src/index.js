b799c03d73d4c553481b0733992ecf0b28d80586
Jem Alvarez <jemalvarezcontact@gmail.com>
2019-03-26 10:29:40 -0400

    Fix CORS 2

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT
-
-app.use(express.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
