54836daf528132888cb2eb58fe5d9d8ac3c977c0
Jem Alvarez <jemalvarezcontact@gmail.com>
2019-04-09 12:14:11 -0400

    -

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-const app = require('./app')
+// const app = require('./app')
+const express = require('express')
+require('./db/mongoose')
+const cors = require('cors')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 const port = process.env.PORT
 
